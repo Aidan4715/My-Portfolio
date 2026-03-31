102624e36b66ca38dd9cc8320826cfe47865e69b
Joshi <aidanjoshi@csu.fullerton.edu>
2026-03-30 20:23:38 -0700

    cleared up the files so i can make the website

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {} from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolv: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
